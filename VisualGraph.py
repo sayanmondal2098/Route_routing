a4c539bff2a7a0013b8155535fa7c674bd8fc2b4
Sayan Mondal <mondalsayan556@gmail.com>
2019-11-20 12:29:12 +0530

    primary node properties seted

--- VisualGraph.py
+++ VisualGraph.py
@@ -11,35 +11,23 @@ class Graph :
     # ,_data=None,_size=None,_font=None,_sourcepos=None,_destination=None
     _size = 0
     def __init__(self):
-        self._data = [None]*10
-        # self._size = 0
-        self._font = 0
-        self._sourcepos = 0
-        self._destination = 0
+        self._data = []
     
     def _addNode(self,w,x,y,z):
         # if (self._size)==len(self._data):
         #     self._resize(2*len(self.data))
         # avail = (self._font + self._size)%len(self._data)
         # avail = self._size
-        self._data[0] = self._Node(w,x,y,z)
+        self._data.append(self._Node(w,x,y,z))
         self._size += 1
 
     def _getLength(self):
         return self._size
 
-    def _resize(self,cap):
-        old = self._data
-        self._data = [None]*cap
-        walk = self._font
-        for k in range(self._size):
-            self._data[k]=old[walk]
-            walk = (1+walk)%len(old)
-            self._font = 0
 
+G = Graph()
+G._addNode(1,1,1,1)
 
-Graph._addNode(1,1,1,1,1)
+G._addNode(1,1,1,1)
 
-Graph._addNode(1,1,1,1,1)
-
-print(Graph._getLength())
+print(G._getLength())
