9656a1a130d078c1a7d8b5a3bb44c833e7df6fbc
Sayan Mondal <sayanmondal2098@gmail.com>
2019-11-29 22:51:26 +0530

    Probalistic function added

--- Tester1.py
+++ Tester1.py
@@ -1,8 +1,8 @@
 def probability(num,den): 
+    if den==0: return 0
     return num/den
 def matrixEdges(matrx,n):
     edges = dict()
-    mylist = []
     for i in range(n):
         for j in range(n):
             var = matrix[i][j]
@@ -11,18 +11,20 @@ def matrixEdges(matrx,n):
             if i+1<n: edges[var].append(matrix[i+1][j])
             if j-1>=0: edges[var].append(matrix[i][j-1])
             if j+1<n: edges[var].append(matrix[i][j+1])
+    prob = dict()
     for pt1 in edges:
-        den = len(edges[pt1])
-        
+        if pt1 not in prob: prob[pt1] = dict()
+        den1 = len(edges[pt1])
         for pt2 in edges[pt1]:
-            print(pt1,"-",pt2,"Probability: ", probability(1,den))
-            pp = (pt1,pt2)
-            mylist.append(pp)
-            
-    # print(edges)
-    print(mylist)
-
-n = int(input())
+                if pt2 not in prob: prob[pt2] = dict()
+                den2 = len(edges[pt2])
+                temp = (probability(1,den1)+probability(1,den2))/2
+                prob[pt1][pt2] = temp
+                prob[pt2][pt1] = temp
+    for pt1 in prob:
+        for pt2 in prob[pt1]: print(pt1,"-",pt2,"Probability :",prob[pt1][pt2])
+    #print(edges)
+n = int(input("Enter N : "))
 matrix = []
 val = 1
 for i in range(n):
